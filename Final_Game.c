4b10f171558b0d50f9ef4d1f2a515f147b2f7573
Piyush288a <piyush288a@gmail.com>
2025-06-20 14:14:19 +0530

    CHange

--- Final_Game.c
+++ Final_Game.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+ //cd "d:\PROJECTS\Sudoku Game\" ; if ($?) { g++ Final_Game.cpp ScrollEffect.cpp -o Final_Game } ; if ($?) { .\Final_Game }
+ #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
